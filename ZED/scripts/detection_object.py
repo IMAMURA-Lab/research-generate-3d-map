f93eda1dd64ab5a77f03c304db9e980d8f1b51f9
hiromu <murohi.kato.study@gmail.com>
2025-12-10 14:08:01 +0900

    fix

--- ZED/scripts/detection_object.py
+++ ZED/scripts/detection_object.py
@@ -30,11 +30,7 @@ def main():
     CLASS_COLORS = {
         "fire": (0, 0, 255),         # 赤
         "person": (255, 255, 0)      # 黄色
-<<<<<<< HEAD
     }
-=======
-}
->>>>>>> ae27925255ca5735441524a3f27c64116bd316d1
 
     # -----------------------------
     # ZED 初期化
@@ -52,11 +48,7 @@ def main():
     runtime_params = sl.RuntimeParameters()
 
     image = sl.Mat()
-<<<<<<< HEAD
     point_cloud = sl.Mat()
-=======
-    point_cloud_zed = sl.Mat()
->>>>>>> ae27925255ca5735441524a3f27c64116bd316d1
 
     while True:
 
@@ -65,11 +57,7 @@ def main():
             continue
 
         zed.retrieve_image(image, sl.VIEW.LEFT)
-<<<<<<< HEAD
         zed.retrieve_measure(point_cloud, sl.MEASURE.XYZRGBA)
-=======
-        zed.retrieve_measure(point_cloud_zed, sl.MEASURE.XYZRGBA)
->>>>>>> ae27925255ca5735441524a3f27c64116bd316d1
 
         # ZED の Mat を OpenCV 形式に変換（# NumPy 配列として取得）
         image_ocv = np.array(image.get_data(), dtype=np.uint8, copy=True)
@@ -82,10 +70,6 @@ def main():
         # YOLOv8 推論
         # -----------------------------
         results = model.predict(image_ocv, conf=CONF_THRESH, verbose=False)
-<<<<<<< HEAD
-=======
-        names = results[0].names
->>>>>>> ae27925255ca5735441524a3f27c64116bd316d1
 
         for r in results:
             for box in r.boxes:
@@ -110,7 +94,7 @@ def main():
                 # -----------------------------
                 # 深度取得
                 # -----------------------------
-                err, point = point_cloud_zed.get_value(cx, cy)
+                err, point = point_cloud.get_value(cx, cy)
                 if err != sl.ERROR_CODE.SUCCESS:
                     continue
 
@@ -121,7 +105,6 @@ def main():
                 # -----------------------------
                 # 描画
                 # -----------------------------
-<<<<<<< HEAD
 
                 color = CLASS_COLORS.get(class_name, (255, 255, 255))  # デフォルトは白
 
@@ -130,13 +113,6 @@ def main():
                 cv2.putText(image_ocv, label, (xmin, ymin - 10),
                             cv2.FONT_HERSHEY_SIMPLEX, 0.7, color, 1)
 
-=======
-                cv2.rectangle(image_ocv, (xmin, ymin), (xmax, ymax), (0, 255, 0), 2)
-                label = f"{class_name} {conf:.2f}  dist:{distance:.2f}m"
-                cv2.putText(image_ocv, label, (xmin, ymin - 10),
-                            cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 255, 0), 2)
-
->>>>>>> ae27925255ca5735441524a3f27c64116bd316d1
         cv2.imshow("Viewew [detection_object]", image_ocv)
 
         if cv2.waitKey(1) == 27:  # ESC キー
