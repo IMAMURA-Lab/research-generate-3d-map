718a149ea930e2b0f1dd6e769dbb60da3a86fd28
Hiromu <murohi.kato.study@gmail.com>
2026-07-24 03:39:45 +0900

    modified ZED dir

--- ZED/tests/test_pipeline.py
+++
@@ -1,8 +0,0 @@
-"""rescue_pipeline のテスト雛形(結合テスト)。"""
-
-import pytest
-
-
-def test_pipeline_produces_mesh_and_objects_files(tmp_path):
-    """パイプライン実行後、mesh.objとobjects.jsonが出力されることを確認する。"""
-    pass
